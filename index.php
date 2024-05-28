762260772388cf2dcbbd2a528155194b3888497f
Oussama-Kahlaoui <ousama.kahlaoui@gmail.com>
2024-05-28 17:54:34 +0100

    Changes

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
 <?php
 $quantity=4;
 $food= "pizza";
+$price= 5.49;
 $total=null;
 
 
-echo "you have {$food}"
+echo "you have orderd {$quantity} x {$food} <br> ";
+
+
+$total = $quantity * $price;
+echo "You re total is {$total}\$"
+
+
+
 
 
 ?>
