c3d77da03bb606edc416cd59b95eab27fce9d1f3
Oussama-Kahlaoui <ousama.kahlaoui@gmail.com>
2024-05-28 18:28:48 +0100

    new change

--- index.php
+++ index.php
@@ -1,17 +1,23 @@
 <?php
-$quantity=4;
-$food= "pizza";
-$price= 5.49;
-$total=null;
-$age=21;
+$x= 10;
+$y= 3;
+
+$z=null;
+
+// $z = $x + $y;
+// $z = $x - $y;
+// $z = $x * $y;
+// $z = $x / $y;
+// $z = $x ** $y;
+$z = $x % $y;
 
 
-echo "you have orderd {$quantity} x {$food} <br> ";
 
 
-$total = $quantity * $price;
-echo "You re total is {$total}\$ <br>";
-echo "welcome "
+
+
+echo $z
+
 
 
 
