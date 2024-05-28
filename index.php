ffca294960c9440562ffb7f87fb6667d988676ff
Oussama-Kahlaoui <ousama.kahlaoui@gmail.com>
2024-05-28 18:03:28 +0100

    add ligne

--- index.php
+++ index.php
@@ -3,13 +3,15 @@ $quantity=4;
 $food= "pizza";
 $price= 5.49;
 $total=null;
+$age=21;
 
 
 echo "you have orderd {$quantity} x {$food} <br> ";
 
 
 $total = $quantity * $price;
-echo "You re total is {$total}\$"
+echo "You re total is {$total}\$ <br>";
+echo "welcome "
 
 
 
